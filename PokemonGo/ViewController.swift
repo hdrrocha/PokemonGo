f73e3bb1d23082e1a3d9822b0f9550fa14afcafb
Hédie Rocha <hedie.strokes@gmail.com>
2018-03-01 14:33:48 -0300

    Add files via upload

---
+++ PokemonGo/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  PokemonGo
+//
+//  Created by Helder Rocha on 01/03/18.
+//  Copyright © 2018 Helder Rocha. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //quem via vai gerenciar esse mapa é essa propria class
+        mapa.delegate = self
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
