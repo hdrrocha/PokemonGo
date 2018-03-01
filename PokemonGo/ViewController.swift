d410dfe48605c472356793f9ce7e0f12700674e5
Helder Rocha <hedie.strokes@gmail.com>
2018-03-01 14:59:21 -0300

    user permition

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -25,6 +25,27 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse && status != .notDetermined{
+            //alert
+            let alertController = UIAlertController(title: "Permissão de localização", message: "Para que você possa caçar pokemons é necessario que  autorize o uso de localização", preferredStyle: .alert)
+            
+            let acaoConfiguracao = UIAlertAction(title: "Abrir configurações", style: .default, handler: {
+                (alertaConfiguracoes) in
+                if let configuracoes = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+            })
+            
+            let acaoCancelar =  UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertController.addAction(acaoConfiguracao)
+            alertController.addAction(acaoCancelar)
+            
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 
 
 }
